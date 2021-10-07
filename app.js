e1be3390bae5f51f3ded98113f109ccd63161d34
KOMUYA <komuyaproyecto2021@gmail.com>
2021-10-07 12:28:25 -0500

    Página terminada

---
+++ app.js
@@ -0,0 +1,63 @@
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+});
+
+sr.reveal('.principal',{}); 
+sr.reveal('.logo',{delay: 200}); 
+sr.reveal('.titulo',{delay: 400}); 
+
+sr.reveal('.section02',{}); 
+sr.reveal('.text2',{delay: 400}); 
+sr.reveal('.counter',{delay: 400}); 
+sr.reveal('.box5',{delay: 400});
+
+
+sr.reveal('.section03',{}); 
+sr.reveal('.titleSection3',{delay: 400}); 
+sr.reveal('.titleI',{delay: 400});
+sr.reveal('.titleE',{delay: 400});
+
+
+
+
+const srl = ScrollReveal({
+  origin: 'left',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+});
+
+
+srl.reveal('.section03',{}); 
+srl.reveal('.box1',{delay: 400}); 
+srl.reveal('.box2',{}); 
+srl.reveal('.left',{delay: 400}); 
+srl.reveal('.box6',{delay: 400}); 
+
+
+
+
+const srr = ScrollReveal({
+  origin: 'right',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+});
+
+srr.reveal('.image2',{delay: 200}); 
+srr.reveal('.box2',{}); 
+srr.reveal('.right',{delay: 400}); 
+
+
+const srd = ScrollReveal({
+  origin: 'bottom',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+});
+
+srd.reveal('.center',{delay: 400}); 
+srd.reveal('.imageEncu',{delay: 400}); 
