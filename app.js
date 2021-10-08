59ec29da28016fa1af3cd9e6b6f363f13585c46d
KOMUYA <komuyaproyecto2021@gmail.com>
2021-10-07 19:46:37 -0500

    fix

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ sr.reveal('.section02',{});
 sr.reveal('.text2',{delay: 400}); 
 sr.reveal('.counter',{delay: 400}); 
 sr.reveal('.box5',{delay: 400});
+sr.reveal('.box6',{delay: 400});
 
 
 sr.reveal('.section03',{}); 
@@ -22,42 +23,17 @@ sr.reveal('.titleE',{delay: 400});
 sr.reveal('.imageEncu',{delay: 200}); 
 
 sr.reveal('.options',{}); 
+sr.reveal('.section03',{}); 
+sr.reveal('.box1',{delay: 400}); 
+sr.reveal('.box2',{}); 
+sr.reveal('.left',{delay: 400}); 
 
 
-
-const srl = ScrollReveal({
-  origin: 'left',
-  distance: '90px',
-  duration: 2000,
-  reset: true
-});
-
-
-srl.reveal('.section03',{}); 
-srl.reveal('.box1',{delay: 400}); 
-srl.reveal('.box2',{}); 
-srl.reveal('.left',{delay: 400}); 
+sr.reveal('.image2',{delay: 200}); 
+sr.reveal('.right',{delay: 400}); 
 
 
 
 
-const srr = ScrollReveal({
-  origin: 'right',
-  distance: '80px',
-  duration: 2000,
-  reset: true
-});
-
-srr.reveal('.image2',{delay: 200}); 
-srr.reveal('.box2',{}); 
-srr.reveal('.right',{delay: 400}); 
-
-
-const srd = ScrollReveal({
-  origin: 'bottom',
-  distance: '80px',
-  duration: 2000,
-  reset: true
-});
 
 sr.reveal('.center',{delay: 400}); 
